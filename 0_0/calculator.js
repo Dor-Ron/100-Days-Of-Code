8887db13a9d25c3f47e3f4ad610dbcfc40ed9818
Dor Rondel <dor.rondel@gmail.com>
2016-07-22 14:55:13 -0400

    converted 0_0 code to abide to the object literal pattern

--- 0_0/calculator.js
+++ 0_0/calculator.js
@@ -1,24 +1,44 @@
-const num1 = document.getElementsByName('num1')[0];
-const operator = document.getElementsByName('op')[0];
-const num2 = document.getElementsByName('num2')[0];
-const sum = document.getElementById('sum');
+(() => {
+  const calculator = {
+    init: function(){
+      this.cacheDOM();
+      this.bindEvent();
+      console.log(typeof this.getValue);
+    },
+    cacheDOM: function() {
+      this.num1 = document.getElementsByName('num1')[0];
+      this.operator = document.getElementsByName('op')[0];
+      this.num2 = document.getElementsByName('num2')[0];
+      this.sum = document.getElementById('sum');
+    },
+    bindEvent: function() {
+      this.num1.addEventListener("keyup", this.calculate.bind(this));
+      this.num2.addEventListener("keyup", this.calculate.bind(this));
+      this.operator.addEventListener("keyup", this.calculate.bind(this));
+    },
+    getValue: function() {
+      this.first = this.num1.value;
+      this.second = this.num2.value;
+      this.op = this.operator.value;
+    },
+    calculate: function(){
+      this.solution = 0;
+      this.getValue();
+      if (this.op === "+") {
+          this.solution = parseFloat(this.first) + parseFloat(this.second);
+      } else if (this.op === "-") {
+        this.solution = parseFloat(this.first) - parseFloat(this.second);
+      } else if (this.op === "*") {
+        this.solution = parseFloat(this.first) * parseFloat(this.second);
+      } else {  // Without error-handling, only option left should be division
+        this.solution = parseFloat(this.first) / parseFloat(this.second);
+      }
+      this.render();
+    },
+    render: function(){
+      this.sum.innerHTML = "Solution: " + this.solution;
+    }
+  };
 
-num1.addEventListener("keyup", doMath);
-num2.addEventListener("keyup", doMath);
-operator.addEventListener("keyup", doMath);
-
-function doMath() {
-  var first = num1.value;
-  var second = num2.value;
-  var op = operator.value;
-  var solution;
-  if (op === "+") {
-    solution = parseFloat(first) + parseFloat(second);
-  } else if (op === "-") {
-    solution = parseFloat(first) - parseFloat(second);
-  } else if (op === "*") {
-    solution = parseFloat(first) * parseFloat(second);
-  } else {  // Without error-handling, only option left should be division
-    solution = parseFloat(first) / parseFloat(second);
-  } sum.innerHTML = "Solution: " + solution;
-}
+  calculator.init();
+})();
